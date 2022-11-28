3005a6d9a783079eadf77c89ead55d2596343054
Pedro A. González <pgonzalez@post.com>
2022-11-28 12:39:13 -0500

    Add lecture code web servers 101, install morgan

--- w03d1/express-server.js
+++ w03d1/express-server.js
@@ -1,7 +1,18 @@
+// -------------------- REQUIREMENTS
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+// -------------------- MIDDLEWARE
+app.use((req, res, next) => {
+  console.log(`METHOD: ${req.method} URL: ${req.url}`);
+  next();
+});
+
+app.use(morgan('dev'));
+
+// -------------------- ROUTES/ENDPOINTS
 app.get('/', (req, res) => {
   res.send('Hello World from express!');
 });
@@ -11,10 +22,20 @@ app.get('/pedro', (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-  const users = ['abdul', 'dihni', 'nawal'];
+  const users = ['abdul', 'dihni', 'nawal', 'rohan'];
   res.send(users);
 });
 
+app.get('/welcome', (req, res) => {
+  const { name } = req.query;
+  if (!name) {
+    return res.status(400).send('Name query not provided');
+  }
+
+  res.send(`Welcome ${name}`);
+});
+
+// -------------------- LISTENER
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
