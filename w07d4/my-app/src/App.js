41469d26c9feeb40277276dab366e83e792849d8
Pedro A. González <pgonzalez@post.com>
2023-01-12 14:20:31 -0500

    Add api call as a data fetching side effect

--- w07d4/my-app/src/App.js
+++ w07d4/my-app/src/App.js
@@ -2,22 +2,39 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const [results, setResults] = useState([]);
 
-  const handleClick = () => setCount(count + 1);
+  const handleDecrement = () => count > 1 && setCount(count - 1);
+  const handleIncrement = () => setCount(count + 1);
 
   useEffect(() => {
-    document.title = `Counter: ${count}`;
+    document.title = `Rick and Morty Page: ${count}`;
   }, [count]);
 
   useEffect(() => {
-    console.log(`Counter: ${count}`);
+    fetch(`https://rickandmortyapi.com/api/character?page=${count}`)
+      .then((res) => res.json())
+      .then((data) => setResults(data.results))
+      .catch((err) => console.log(err));
   }, [count]);
 
   return (
     <div>
-      <h2>Counter: {count}</h2>
-      <button onClick={handleClick}>Increment</button>
+      <h1>Rick and Morty Characters!</h1>
+      <h2>Page: {count}</h2>
+      <button onClick={handleDecrement}>Prev</button>
+      <button onClick={handleIncrement}>Next</button>
+      <br />
+      <ul>
+        {results.map((char) => (
+          <li key={char.id}>
+            <img width="100px" src={char.image} alt={char.name} />
+            <h4>{char.name}</h4>
+            <p>{char.species}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
